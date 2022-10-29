b06d754634f52ee6b2a6c20111f4e3370ae3287a
Ali-Afifi <ali.m.afify2001@gmail.com>
2022-10-29 13:26:33 +0200

    adding the signal generator from the main branch

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import streamlit as st
 import utils as utl
-from views import home,about,analysis,options,configuration
+from views import home, about, analysis, options, configuration
 
 st.set_page_config(layout="wide", page_title='Navbar sample')
 st.set_option('deprecation.showPyplotGlobalUse', False)
 utl.inject_custom_css()
 utl.navbar_component()
 
+
 def navigation():
     route = utl.get_current_route()
-    if route == None:
+    if route == None or route == "home":
         home.load_view()
     elif route == "about":
         about.load_view()
@@ -19,5 +20,6 @@ def navigation():
         options.load_view()
     elif route == "configuration":
         configuration.load_view()
-    
+
+
 navigation()
