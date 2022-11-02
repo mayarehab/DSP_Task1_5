bc31db315fd597820d1ef0d4e897f12879b6986c
Ali-Afifi <ali.m.afify2001@gmail.com>
2022-11-02 21:34:45 +0200

    inital fix

--- app.py
+++ app.py
@@ -3,79 +3,75 @@ import pandas as pd
 import pandas as ps
 import numpy as np
 import plotly_express as px
+import plotly.graph_objects as go
 
 from numpy import tile, dot, newaxis, sinc
-import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
-col1 , col2  = st.columns([10, 2])
+col1, col2 = st.columns([10, 2])
 units = {
-        "Hz (hertz)": 1,
-        "kHz (kilohertz)": 1000,
+    "Hz (hertz)": 1,
+    "kHz (kilohertz)": 1000,
 
-         }
+}
 signals = []
 Fig = go.Figure()
 
 if "frequencies" not in st.session_state:
-        st.session_state["frequencies"] = []
+    st.session_state["frequencies"] = []
 
 if "amplitudes" not in st.session_state:
-        st.session_state["amplitudes"] = []
+    st.session_state["amplitudes"] = []
 if "time" not in st.session_state:
-        st.session_state["time"] = np.arange(0, np.pi, 0.0001)
+    st.session_state["time"] = np.arange(0, 10, 0.0001)
 
 if "signal" not in st.session_state:
-        st.session_state["signal"] = np.zeros(len(st.session_state["time"]))
-        
+    st.session_state["signal"] = np.zeros(len(st.session_state["time"]))
 
 
-
-
-
-def add_signal_plotting( amp):
+def add_signal_plotting(amp):
     for i in range(len(st.session_state["amplitudes"])):
         if i == 0:
-            st.session_state["time"] = np.arange(0, np.pi, 0.0001)
-            st.session_state["signal"] = np.zeros(len(st.session_state["time"]))
-            
-        st.session_state["signal"] += st.session_state["amplitudes"][i] * np.sin(2 * np.pi * st.session_state["frequencies"][i] * st.session_state["time"])
+            st.session_state["time"] = np.arange(0, 10, 0.0001)
+            st.session_state["signal"] = np.zeros(
+                len(st.session_state["time"]))
 
-
-   
+        st.session_state["signal"] += st.session_state["amplitudes"][i] * np.sin(
+            2 * np.pi * st.session_state["frequencies"][i] * st.session_state["time"])
 
     if len(st.session_state["amplitudes"]) != 0:
-       
-        fig = px.line(x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), labels={'x': 'Time (seconds)', 'y': 'Amplitude'})
+
+        fig = px.line(x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(
+        ), labels={'x': 'Time (seconds)', 'y': 'Amplitude'})
         Fig.add_trace(go.Scatter(
-        x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), mode='lines', line=go.scatter.Line(color="black")))
+            x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), mode='lines', line=go.scatter.Line(color="dark blue"), name="generated signal"))
     else:
-    
-        st.session_state["time"] = np.arange(0, np.pi, 0.0001)
+
+        st.session_state["time"] = np.arange(0, 10, 0.0001)
         st.session_state["signal"] = np.zeros(len(st.session_state["time"]))
-        fig = px.line(x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), labels={'x': 'Time (seconds)', 'y': 'Amplitude'})
+        fig = px.line(x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(
+        ), labels={'x': 'Time (seconds)', 'y': 'Amplitude'})
 
         Fig.add_trace(go.Scatter(
-        x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), mode='lines', line=go.scatter.Line(color="black")))
-  
+            x=st.session_state["time"].tolist(), y=st.session_state["signal"].tolist(), mode='lines', line=go.scatter.Line(color="dark blue"), name="generated signal"))
 
-    
     st.plotly_chart(fig, use_container_width=True)
 
     return amp
 
 
 def signal_adder(frequency, amplitude):
-        st.session_state["frequencies"].append(frequency)
-        st.session_state["amplitudes"].append(amplitude)
-        
+    st.session_state["frequencies"].append(frequency)
+    st.session_state["amplitudes"].append(amplitude)
+
 
 def signal_deleter(selected_signal):
-        index = signals.index(selected_signal)
-        del st.session_state["frequencies"][index]
-        del st.session_state["amplitudes"][index]
-        st.experimental_rerun()
+    index = signals.index(selected_signal)
+    del st.session_state["frequencies"][index]
+    del st.session_state["amplitudes"][index]
+    st.experimental_rerun()
+
 
 def sample(time, amplitude, fs):
     if len(time) == len(amplitude):
@@ -102,9 +98,9 @@ def sinc_interp(sampled_Amp, sampled_Time, Time):
 
 
 def Plotting(time, signal, samplefreq, recon_time, recon_signal):
-    
+
     Fig.add_trace(go.Scatter(
-        x=time, y=signal, mode='lines', line=go.scatter.Line(color="black")))
+        x=time, y=signal, mode='lines', line=go.scatter.Line(color="black"), name="signal"))
     Fig.update_xaxes(title_text="Time (s)")
     Fig.update_yaxes(title_text="Amplitude (mV)")
     sampled_Time, sampled_Amp = sample(time, signal, samplefreq)
@@ -114,8 +110,6 @@ def Plotting(time, signal, samplefreq, recon_time, recon_signal):
     Fig.add_trace(go.Scatter(
         x=recon_time, y=recon_signal, mode='lines', name='Reconstructed Signal', line=go.scatter.Line(color="grey")))
     st.plotly_chart(Fig, use_container_width=True)
-
-    
 
 
 def PlotRecons(time, Signal):
@@ -127,33 +121,35 @@ def PlotRecons(time, Signal):
 
     st.plotly_chart(Fig, use_container_width=True)
 
-with col2:
-        
-        frequency_units = st.selectbox('Select Frequency units', options=units)
 
-        frequency = st.slider(min_value=1.0, max_value=20.0, step=0.1,
+with col2:
+
+    frequency_units = st.selectbox('Select Frequency units', options=units)
+
+    frequency = st.slider(min_value=1.0, max_value=20.0, step=0.1,
                           label="Frequency")*units[frequency_units]
 
-        amplitude = st.slider(min_value=1.0, max_value=20.0,
+    amplitude = st.slider(min_value=1.0, max_value=20.0,
                           step=0.1, label="Amplitude")
 
-        if st.button(label="Add Signal"):
-            signal_adder(frequency, amplitude)
+    if st.button(label="Add Signal"):
+        signal_adder(frequency, amplitude)
 
-        selected_signal = st.selectbox(label="Choose signal", options=signals)
+    selected_signal = st.selectbox(label="Choose signal", options=signals)
 
-        if st.button(label="Delete Signal"):
-         if len(st.session_state["amplitudes"]) != 0:
-                signal_deleter(selected_signal)
+    if st.button(label="Delete Signal"):
+        if len(st.session_state["amplitudes"]) != 0:
+            signal_deleter(selected_signal)
 
-        signal_csv = pd.DataFrame({"Time": st.session_state["time"].tolist(), "Value": st.session_state["signal"].tolist()}).to_csv(index=False).encode('utf-8')
+    signal_csv = pd.DataFrame({"Time": st.session_state["time"].tolist(
+    ), "Value": st.session_state["signal"].tolist()}).to_csv(index=False).encode('utf-8')
 
-        st.download_button(
+    st.download_button(
         label="Download the signal as CSV",
         data=signal_csv,
         file_name='generated_signal.csv',
         mime='text/csv',
-         )
+    )
 
 uploaded_file = st.sidebar.file_uploader("Upload your file here", type="CSV")
 
@@ -188,22 +184,18 @@ if uploaded_file is not None:
         # Noise up the original signal
         noisedSignal = amp + noise_volts
 
-        
-
         with col1:
             add_signal_plotting()
             sampled_Time, sampled_Amp = sample(time, noisedSignal, fs)
             Plotting(time, noisedSignal, fs, time, sinc_interp(
-            sampled_Amp, sampled_Time, time))
-
-        
+                sampled_Amp, sampled_Time, time))
 
     else:
-       with col1:
-        add_signal_plotting()
-        sampled_Time, sampled_Amp = sample(time, amp, fs)
-        Plotting(time, amp, fs, time, sinc_interp(
-            sampled_Amp, sampled_Time, time))
+        with col1:
+            add_signal_plotting()
+            sampled_Time, sampled_Amp = sample(time, amp, fs)
+            Plotting(time, amp, fs, time, sinc_interp(
+                sampled_Amp, sampled_Time, time))
 
 
 else:
@@ -237,7 +229,7 @@ else:
         with col1:
             add_signal_plotting()
             Plotting(time, noisedSignal, fs, time, sinc_interp(
-            sampled_Amp, sampled_Time, time))
+                sampled_Amp, sampled_Time, time))
 
             # PlotRecons(time, sinc_interp(sampled_Amp, sampled_Time, time))
 
@@ -248,6 +240,6 @@ else:
             # st.header(amp)
             sampled_Time, sampled_Amp = sample(time, amp, fs)
             Plotting(time, amp, fs, time, sinc_interp(
-            sampled_Amp, sampled_Time, time))
+                sampled_Amp, sampled_Time, time))
 
             # PlotRecons(time, sinc_interp(sampled_Amp, sampled_Time, time))
